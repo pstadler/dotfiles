742c5d1bd93f06341a4625598ea4e3a5f2dbfc20
Patrick Stadler <patrick.stadler@gmail.com>
2012-01-04 14:23:30 +0100

    updated install.sh to include ackrc

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for symlink in bash_profile bashrc gitconfig vim vimrc zshrc
+for symlink in bash_profile bashrc gitconfig vim vimrc zshrc ackrc
 do
 	ln -s $PWD/$symlink ~/.$symlink
 done
