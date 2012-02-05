2ed656d2ff7fbea419d5e5a85d19b740ac8d8ba7
Patrick Stadler <patrick.stadler@gmail.com>
2012-02-05 21:00:29 +0100

    added sublime-text-2 config

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ for symlink in bash_profile bashrc gitconfig vim vimrc zshrc ackrc
 do
 	ln -s $PWD/$symlink ~/.$symlink
 done
+
+ln -s ~/GitHub/dotfiles/sublime-text-2/Packages/User ~/Library/Application\ Support/Sublime\ Text\ 2/Packages
