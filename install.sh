fb750b643ac2bb53560e7ed7f29b059351e540f9
Patrick Stadler <patrick.stadler@gmail.com>
2012-02-05 21:07:35 +0100

    install.sh - install "subl" command

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Install dotfiles
 for symlink in bash_profile bashrc gitconfig vim vimrc zshrc ackrc
 do
 	ln -s $PWD/$symlink ~/.$symlink
 done
 
+# Sublime Text 2 Config and symlink
 ln -s ~/GitHub/dotfiles/sublime-text-2/Packages/User ~/Library/Application\ Support/Sublime\ Text\ 2/Packages
+ln -s /Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
