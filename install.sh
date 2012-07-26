2cb7a4e6d475af5be58e3ffe3e7c1042a08f8789
Patrick Stadler <patrick.stadler@gmail.com>
2012-07-26 11:52:12 +0200

    added virtualenv hooks

--- install.sh
+++ install.sh
@@ -3,7 +3,15 @@
 # Install dotfiles
 for symlink in bash_profile bashrc gitconfig vim vimrc zshrc ackrc
 do
+	rm ~/.$symlink
 	ln -s $PWD/$symlink ~/.$symlink
+done
+
+# Install virtualenv hooks
+for symlink in postactivate postdeactivate
+do
+	rm ~/.virtualenvs/$symlink
+	ln -s $PWD/virtualenvs/$symlink ~/.virtualenvs
 done
 
 # Sublime Text 2 Config and symlink
