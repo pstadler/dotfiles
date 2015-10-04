c73bae767a63e84e7b6949f2130645ac523a65ae
Patrick Stadler <patrick.stadler@gmail.com>
2015-10-04 17:56:27 +0200

    hammerspoon: drop hs.grid

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -9,106 +9,83 @@ local mash = {
 
 local spacesModifier = "ctrl"
 
-local gridSize = "2x2"
-local gridMargins = {0, 0}
 local animationDuration = 0
 
-local winCenterRatios = {
+local centeredWindowRatios = {
   small = { w = 0.8, h = 0.8 },
   large = { w = 0.66, h = 0.66 }
 }
 
 
 -- Setup
-hs.grid.setGrid(gridSize, nil)
-hs.grid.setMargins(gridMargins)
 hs.window.animationDuration = animationDuration
 local logger = hs.logger.new("config", "verbose")
 
 
 -- Resize windows
--- top half
-hs.hotkey.bind(mash.split, "up", function()
-  hs.grid.set(hs.window.focusedWindow(), "0,0 2x1")
-end)
-
--- right half
-hs.hotkey.bind(mash.split, "right", function()
-  hs.grid.set(hs.window.focusedWindow(), "1,0 1x2")
-end)
-
--- bottom half
-hs.hotkey.bind(mash.split, "down", function()
-  hs.grid.set(hs.window.focusedWindow(), "0,1 2x1")
-end)
-
--- left half
-hs.hotkey.bind(mash.split, "left", function()
-  hs.grid.set(hs.window.focusedWindow(), "0,0 1x2")
-end)
-
--- top left
-hs.hotkey.bind(mash.corner, "up", function()
-  hs.grid.set(hs.window.focusedWindow(), "0,0 1x1")
-end)
-
--- top right
-hs.hotkey.bind(mash.corner, "right", function()
-  hs.grid.set(hs.window.focusedWindow(), "1,0 1x1")
-end)
-
--- bottom right
-hs.hotkey.bind(mash.corner, "down", function()
-  hs.grid.set(hs.window.focusedWindow(), "1,1 1x1")
-end)
-
--- bottom left
-hs.hotkey.bind(mash.corner, "left", function()
-  hs.grid.set(hs.window.focusedWindow(), "0,1 1x1")
-end)
-
--- top center
-hs.hotkey.bind(mash.split, ".", function()
+local function adjust(x, y, w, h)
   local win = hs.window.focusedWindow()
   if not win then return end
 
   local f = win:frame()
   local max = win:screen():frame()
-  local size = max.w >= 2560 and "large" or "small"
-  f.w = max.w * resetRatios[size].w
-  f.h = max.h * resetRatios[size].h
-  f.x = (max.w / 2) - (f.w / 2)
+
+  f.w = math.floor(max.w * w)
+  f.h = math.floor(max.h * h)
+  f.x = math.floor((max.w * x) + max.x)
+  f.y = math.floor((max.h * y) + max.y)
+
+  win:setFrame(f)
+end
+
+local function adjustCenterTop(w, h)
+  local win = hs.window.focusedWindow()
+  if not win then return end
+
+  local f = win:frame()
+  local max = win:screen():frame()
+
+  f.w = math.floor(max.w * w)
+  f.h = math.floor(max.h * h)
+  f.x = math.floor((max.w / 2) - (f.w / 2))
   f.y = max.y
   win:setFrame(f)
-end)
+end
 
--- top center
+-- top half
+hs.hotkey.bind(mash.split, "up", function() adjust(0, 0, 1, 0.5) end)
+
+-- right half
+hs.hotkey.bind(mash.split, "right", function() adjust(0.5, 0, 0.5, 1) end)
+
+-- bottom half
+hs.hotkey.bind(mash.split, "down", function() adjust(0, 0.5, 1, 0.5) end)
+
+-- left half
+hs.hotkey.bind(mash.split, "left", function() adjust(0, 0, 0.5, 1) end)
+
+-- top left
+hs.hotkey.bind(mash.corner, "up", function() adjust(0, 0, 0.5, 0.5) end)
+
+-- top right
+hs.hotkey.bind(mash.corner, "right", function() adjust(0.5, 0, 0.5, 0.5) end)
+
+-- bottom right
+hs.hotkey.bind(mash.corner, "down", function() adjust(0.5, 0.5, 0.5, 0.5) end)
+
+-- bottom left
+hs.hotkey.bind(mash.corner, "left", function() adjust(0, 0.5, 0.5, 0.5) end)
+
+-- fullscreen
+hs.hotkey.bind(mash.split, ",", function() adjustCenterTop(1, 1) end)
+
+-- top center small
 hs.hotkey.bind(mash.split, ".", function()
   local win = hs.window.focusedWindow()
-  if win == nil then return end
+  if not win then return end
 
-  local f = win:frame()
-  local max = win:screen():frame()
-  local size = max.w >= 2560 and "large" or "small"
-  f.w = max.w * winCenterRatios[size].w
-  f.h = max.h * winCenterRatios[size].h
-  f.x = (max.w / 2) - (f.w / 2)
-  f.y = max.y
-  win:setFrame(f)
-end)
-
--- full screen
-hs.hotkey.bind(mash.split, ",", function()
-  local win = hs.window.focusedWindow()
-  if win == nil then return end
-
-  local f = win:frame()
-  local max = win:screen():frame()
-  f.w = max.w
-  f.h = max.h
-  f.x = max.x
-  f.y = max.y
-  win:setFrame(f)
+  local size = win:screen():frame().w >= 2560 and "large" or "small"
+  adjustCenterTop(centeredWindowRatios[size].w, centeredWindowRatios[size].h)
 end)
 
 
