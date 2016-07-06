b5d1d5d6cf190ab1f29862f702c55983db7f21ef
Patrick Stadler <patrick.stadler@gmail.com>
2016-07-06 13:54:16 +0200

    add battery functionality to hammerspoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -204,9 +204,67 @@ end
 
 if caffeine then
   caffeine:setClickCallback(toggleCaffeine)
-  setCaffeineMenuItem(hs.caffeinate.get("systemIdle"))
 end
 
 hs.hotkey.bind(mash.utils, "c", toggleCaffeine)
 
+
+-- Battery
+local previousPowerSource = hs.battery.powerSource()
+
+function minutesToHours(minutes)
+  if minutes <= 0 then
+    return "0:00";
+  else
+    hours = string.format("%d", math.floor(minutes / 60))
+    mins = string.format("%02.f", math.floor(minutes - (hours * 60)))
+    return string.format("%s:%s", hours, mins)
+  end
+end
+
+function showBatteryStatus()
+  local message
+
+  if hs.battery.isCharging() then
+    local pct = hs.battery.percentage()
+    local untilFull = hs.battery.timeToFullCharge()
+    message = "Charging"
+
+    if untilFull == -1 then
+      message = string.format("%s %.0f%% (calculating...)", message, pct);
+    else
+      message = string.format("%s %.0f%% (%s remaining)", message, pct, minutesToHours(untilFull))
+    end
+  elseif hs.battery.powerSource() == "Battery Power" then
+    local pct = hs.battery.percentage()
+    local untilEmpty = hs.battery.timeRemaining()
+    message = "Battery"
+
+    if untilEmpty == -1 then
+      message = string.format("%s %.0f%% (calculating...)", message, pct)
+    else
+      message = string.format("%s %.0f%% (%s remaining)", message, pct, minutesToHours(untilEmpty))
+    end
+  else
+    message = "Fully charged"
+  end
+
+  hs.alert.show(message)
+end
+
+function batteryChangedCallback()
+  local powerSource = hs.battery.powerSource()
+
+  if powerSource ~= previousPowerSource then
+    showBatteryStatus()
+    previousPowerSource = powerSource;
+  end
+end
+
+hs.battery.watcher.new(batteryChangedCallback):start()
+
+hs.hotkey.bind(mash.utils, "b", showBatteryStatus)
+
+
+-- All set
 hs.alert.show("Hammerspoon!")
