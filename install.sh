97b73795e28f261f75e5a39bb6cc819101e25c05
Patrick Stadler <patrick.stadler@gmail.com>
2018-09-21 14:21:36 +0200

    tweak tmux.conf. add alacritty.yml

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 echo "Installing dotfiles:"
 for symlink in zshrc bash_profile env env_chpwd gitconfig gitignore \
-                vim vimrc ackrc nvmrc hammerspoon tmux.conf
+                vim vimrc ackrc nvmrc hammerspoon tmux.conf alacritty.yml
 do
   echo " symlink: ~/.$symlink"
 	rm ~/.$symlink
