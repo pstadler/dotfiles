3726cba2114a540cb313415d620644f30f5cb84b
Patrick Stadler <patrick.stadler@gmail.com>
2020-09-30 11:01:21 +0200

    fix hammerspoon focus bindings

--- hammerspoon/focus.lua
+++ hammerspoon/focus.lua
@@ -1,5 +1,5 @@
 -- Focus windows
-hs.hotkey.bind(mash.focus, "up", hs.window.focusWindowNorth)
-hs.hotkey.bind(mash.focus, "right", hs.window.focusWindowEast)
-hs.hotkey.bind(mash.focus, "down", hs.window.focusWindowSouth)
-hs.hotkey.bind(mash.focus, "left", hs.window.focusWindowWest)
+hs.hotkey.bind(mash.focus, "up",  hs.window.frontmostWindow().focusWindowNorth)
+hs.hotkey.bind(mash.focus, "right", hs.window.frontmostWindow().focusWindowEast)
+hs.hotkey.bind(mash.focus, "down", hs.window.frontmostWindow().focusWindowSouth)
+hs.hotkey.bind(mash.focus, "left", hs.window.frontmostWindow().focusWindowWest)
